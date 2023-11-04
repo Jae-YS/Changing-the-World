98aed43e52d15bfe70ca708ae9bcd9758f8dcf5c
khanhle <khanhlanle27@gmail.com>
2023-11-04 17:51:31 -0400

    my curriculum

--- edubuddy/client/src/Components/MyResources.js
+++ edubuddy/client/src/Components/MyResources.js
@@ -1,59 +1,72 @@
 import React, { useState } from 'react';
-import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/outline'; // Assuming you are using Heroicons or similar
 
-// A single resource item component
-const ResourceItem = ({ subject, type, description }) => {
-  const [isOpen, setIsOpen] = useState(false); // State to manage if the item is expanded or not
+function MyResources(){
+    const [selected, setSelected] = useState(null)
 
-  return (
-    <div className="resource-item">
-      <div className="resource-item-header" onClick={() => setIsOpen(!isOpen)}>
-        <h3>{subject}</h3>
-        
-        <p>{type}</p>
-        {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-      </div>
-      {isOpen && (
-        <div className="resource-item-content">
-          <p>{description}</p>
+
+    const toggle = (id) => {
+        setSelected(selected === id ? null : id);
+    }
+
+    return (
+        <div>
+            <h1>My Curriculum</h1>
+            <div className='accordion'>
+                {data.map((block, blockIndex) => (
+                    <div key={blockIndex}>
+                        <h2>{block.date}</h2>
+                        {block.details.map((item, itemIndex) => {
+                            // Create a unique ID for each item
+                            const id = `${blockIndex}-${itemIndex}`;
+                            return (
+                                <div className='item' key={id}>
+                                    <div className="title" onClick={() => toggle(id)}>
+                                        <h3>{item.title}</h3>
+                                        <span>{selected === id ? '-' : '+'}</span>
+                                    </div>  
+                                    {selected === id && (
+                                        <div className='contentshow'>
+                                            <p>{item.description}</p>
+                                            <button>{item.button}</button>
+                                        </div>
+                                    )}
+                                </div>
+                            );
+                        })}
+                    </div>
+                ))}
+            </div>
         </div>
-      )}
-    </div>
-  );
-};
-
-// The MyResources component
-const MyResources = () => {
-  // Sample data structure for resources
-  const resourcesData = [
+    );
+}
+const data = [
     {
-      date: "Monday, Nov 4, 2023",
-      details: [
-        { subject: "Reading & Writing", type: "Homework", description: "Description for Reading & Writing Homework" },
-        { subject: "Science", type: "Creative Writing", description: "Creative Writing - Imagine you find a magic lamp..." },
-        // ... other items
-      ],
+        date: "Monday, Nov 4, 2023",
+        details: [
+            {
+            title: "Reading",
+            description: 'Something working Is this actually',
+            button: 'Start'
+        },
+        {   title: "Science", 
+            description: "Creative Writing - Imagine you find a magic lamp...", 
+            button: "Start" },
+        ]
+        
     },
-    // ... other days
-  ];
-
-  return (
-    <div className="my-resources">
-      {resourcesData.map((day, dayIndex) => (
-        <div key={day.date} className="day-resources">
-          <h2>{day.date}</h2>
-          {day.details.map((item, itemIndex) => (
-            <ResourceItem
-              key={`${dayIndex}-${itemIndex}`}
-              subject={item.subject}
-              type={item.type}
-              description={item.description}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
+    {
+        date: "Monday, Nov 4, 2023",
+        details: [
+            {
+                title: "Reading",
+                description: 'Something working Is this actually',
+                button: 'Start'
+            },
+            {   title: "Science", 
+                description: "Creative Writing - Imagine you find a magic lamp...", 
+                button: "Start" },
+        ]
+    }
+]
 
 export default MyResources;
