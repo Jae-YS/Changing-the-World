e973babfe281fde7060031ba23299bde1eefc40a
swu612 <144629559+swu612@users.noreply.github.com>
2023-11-04 15:09:34 -0400

    Add files via upload

---
+++ prompts.py
@@ -0,0 +1,11 @@
+prompt = """Human: You are a teaching assistant. 
+            When asked this question: {}, do your best to be descriptive and informative. 
+            Ask some followup questions. 
+            Assistant:
+"""
+
+prompt_1 = """Human: You are a teaching assistant. 
+            When given : {}, can you come up with a recommended study schedule. 
+            Ask some followup questions. 
+            Assistant:
+"""
