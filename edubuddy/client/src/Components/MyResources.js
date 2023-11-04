36c0e8adb5e710b894e72f5877191537c6eaeda3
khanhle <khanhlanle27@gmail.com>
2023-11-04 16:47:48 -0400

    delete files

--- edubuddy/client/src/Components/MyResources.js
+++ edubuddy/client/src/Components/MyResources.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/outline'; // Assuming you are using Heroicons or similar
 
-function MyResources() {
-    return (
-        <div>
-            <h1>My Resources</h1>
+// A single resource item component
+const ResourceItem = ({ subject, type, description }) => {
+  const [isOpen, setIsOpen] = useState(false); // State to manage if the item is expanded or not
+
+  return (
+    <div className="resource-item">
+      <div className="resource-item-header" onClick={() => setIsOpen(!isOpen)}>
+        <h3>{subject}</h3>
+        
+        <p>{type}</p>
+        {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+      </div>
+      {isOpen && (
+        <div className="resource-item-content">
+          <p>{description}</p>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
-export default MyResources
+// The MyResources component
+const MyResources = () => {
+  // Sample data structure for resources
+  const resourcesData = [
+    {
+      date: "Monday, Nov 4, 2023",
+      details: [
+        { subject: "Reading & Writing", type: "Homework", description: "Description for Reading & Writing Homework" },
+        { subject: "Science", type: "Creative Writing", description: "Creative Writing - Imagine you find a magic lamp..." },
+        // ... other items
+      ],
+    },
+    // ... other days
+  ];
 
+  return (
+    <div className="my-resources">
+      {resourcesData.map((day, dayIndex) => (
+        <div key={day.date} className="day-resources">
+          <h2>{day.date}</h2>
+          {day.details.map((item, itemIndex) => (
+            <ResourceItem
+              key={`${dayIndex}-${itemIndex}`}
+              subject={item.subject}
+              type={item.type}
+              description={item.description}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MyResources;
