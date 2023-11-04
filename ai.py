e9605e55de928e10c00c460c6fbbfb488d1bf2cb
swu612 <144629559+swu612@users.noreply.github.com>
2023-11-04 15:08:34 -0400

    Add files via upload

--- ai.py
+++ ai.py
@@ -1,58 +1,49 @@
-import requests
-import google.auth
-import google.auth.transport.requests
-from google.oauth2 import service_account
-
-# Define the required SCOPES
-SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
-
-# Replace 'NAME_OF_FILE' with your service account JSON file name that you downloaded
-SERVICE_ACCOUNT_FILE = 'symbolic-math-404116-2d53093f8c72.json'
-
-# Load credentials from the service account file with the specified SCOPES
-cred = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-# Create an authentication request
-auth_req = google.auth.transport.requests.Request()
-
-# Refresh the credentials
-cred.refresh(auth_req)
-
-# Obtain the bearer token
-bearer_token = cred.token
-
-# Define the base URL for your specific region (us-east1 in this example)
-base_url = "https://us-east1-aiplatform.googleapis.com/v1beta1/projects/{project_id}/locations/us-east1/endpoints/{endpoint_id}:predict"
-
-# Replace 'awesome-dogfish-399811' with your GCP project ID
-project_id = "symbolic-math-404116"
-
-# Replace '639689267970310144' with the Endpoint ID from the model dashboard
-endpoint_id = "1798229276995092480"
+import boto3
+import json
+from prompts import prompt
 
 
-# Define the request body for your specific prompt and parameters
-request_body = {
-    "instances": [
-        {
-            "prompt": "Write a poem about Valencia.",
-            "max_length": 200,
-            "top_k": 10
-        }
-    ]
-}
+#Replace these values with the matching credentials upon usage
+region_name = "us-east-1"
+aws_access_key_id = "ASIA3CW6ONM4MU5CZCOO"
+aws_secret_access_key = "GyagSQ8C9efNPBgW7qIvcREph5d7KVxUC1F4uRnF"
+aws_session_token = "IQoJb3JpZ2luX2VjECoaCXVzLWVhc3QtMSJIMEYCIQCo9meNndzrFCfsMyW7vplHSlLmhH4XnOl6evjVgFJeQQIhAJ5rOWUp51rJ9NfBBNXMBtVI111g2oCh5UY79HZ3A7k9KqUDCGMQABoMNzYxNzQ5NDY1OTEyIgyCkV7iy6Lm42ntikwqggMVcG6TWqrvmJ8LiadtwMWq5EBhlcwM3lml1VbMfIplQI/YiKkoBX5zBviUsBnZv1ZQ7altBSDDFt6Zvn1qegnj8HOz2ZwjKR7YLTvcdUAkJfuLb2BAgtuLQSq9JkQIYVMW78PZG9lJqRlXUpWbXiE/QlMiZemzEwLchKl8XMH1KN67jVVewj+sarPBxCBt8g/qFcIZl7QfyfEvfAK8T1tW6F4XEFwap+DQVkaZeYY0PPAVW2aABirdK4jZm30aHHu/+uf94DhL76qNa+6ll9epzQ10dcoAYGnHM4T1Gg8IIz4GxxiM6n5IRDw4ErpgI7Hvc4D7cZnTV+d+TIYclY9cvgd6udzeOXY4PK4dGGWQa7cE4NBcq6kY3Sk3yILaRXzsfhgfiOKsXQxat/lCEsjYzbrI759Qg3u2ORJnfZtY2Fv2duUfHiMRY85JlnUGcJauqcPPUsT2jkkga21MKhusAPa/mnDZo8u4AOfTPIuhnAX7MpW73yNgafllWh+N2tu58TD7jZqqBjqlAU4HtKeqPD6iqOJgytk7Y5Jg2CM3yMOVlY4JS0rMxaRwcjfKhsxAnONrB7Vi9L4/r9sZQhJ1nURb30RFpSGYr/2rUYoO+1ZTV6hbiMqWP1HUfYVebuNkHRe3n4vq/CFCRCYV1gmQJQwFanWeyobobvTkUysAbB+oRDV7QEe4P9ggVSDWca6ys9vjtzyiLRXza9/cwzy3A/22DS78J9v4i5VDQ9MHEA=="
 
-# Create the full URL using the project and endpoint IDs
-full_url = base_url.format(project_id=project_id, endpoint_id=endpoint_id)
+#Input the required credentials to access bedrock
+session = boto3.Session (
+	region_name = region_name,
+	aws_access_key_id = aws_access_key_id,
+	aws_secret_access_key = aws_secret_access_key,
+	aws_session_token = aws_session_token
+)
 
-headers = {
-    "Authorization": "Bearer {bearer_token}".format(bearer_token=bearer_token),
-    "Content-Type": "application/json"
-}
+bedrock = session.client('bedrock-runtime') #Initializing our bedrock connection
 
-# Send a POST request to the model endpoint
-resp = requests.post(full_url, json=request_body, headers=headers)
+#Can swap the modelId to use other AWS Bedrock models as well
+modelId = 'anthropic.claude-v2'
+accept = 'application/json'
+contentType = 'application/json'
 
-# Print the response from the model
-print(resp.json())
+def query(input):
+    body = json.dumps({
+        "prompt": prompt.format(input),
+        "max_tokens_to_sample": 4096,
+        "temperature": 0.5,
+        "top_k": 250,
+        "top_p": 0.5,
+        "stop_sequences": ["\n\nHuman:"]
+    })
+
+    response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+    response_body = json.loads(response.get('body').read())
+    return response_body.get('completion')
+
+print("Welcome to the virtual teaching assistant. To quit, type \"quit\".") 
+
+user_query = ""
+while True: 
+    user_query = input("\rType your response here:")
+    if user_query == "quit":
+        break
+    result = query(user_query)
+    print(result)
