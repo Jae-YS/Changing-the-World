c29f92956ebbacafeacad1281de1bfdeeec7f331
swu612 <144629559+swu612@users.noreply.github.com>
2023-11-04 18:14:18 -0400

    Update wolfram.py

--- edubuddy/AI/wolfram.py
+++ edubuddy/AI/wolfram.py
@@ -4,18 +4,20 @@ client = wolframalpha.Client(app_id)
 
 # def query(input: str) -> str:
 def w_query(input: str) -> str:
+    res = ""
     result = client.query(input)
     for pod in result.pods:
-        print(pod.title)
+        res = res + pod.title
         for sub in pod.subpods:
-            print(sub.plaintext)
+            res = res + sub.plaintext
+    return res
 
 def step_by_step(input: str) -> str:
+    res = ""
     result = client.query(input, params={'podstate': 'Step-by-step solution'})
     for pod in result.pods:
-        print(pod.title)
+        res = res + pod.title
         for sub in pod.subpods:
             if 'plaintext' in sub:
-                print(sub.plaintext)
-            if 'img' in sub:
-                print(sub.img)
+                res = res + sub.plaintext 
+    return res 
