765e4b8d15bc6e6e314e23798df37a15015a9fbb
Jae Young Seo <111537908+Jae-YS@users.noreply.github.com>
2023-11-05 11:54:17 -0500

    Update wolfram.py

--- edubuddy/AI/wolfram.py
+++ edubuddy/AI/wolfram.py
@@ -1,16 +1,19 @@
 import wolframalpha
+import requests
 
-app_id = 'JT5UGJ-YQRQPR8RP2'
+app_id = 'WOLFRAM_APP_ID
 client = wolframalpha.Client(app_id)
 
 # def query(input: str) -> str:
-def w_query(input: str) -> str:
+def w_query(query: str) -> str:
     res = ""
-    result = client.query(input)
+    result = client.query(query)
     for pod in result.pods:
-        res = res + pod.title
+        if pod.title:
+            res = res + pod.title
         for sub in pod.subpods:
-            res = res + sub.plaintext
+            if sub.plaintext:
+                res = res + sub.plaintext
     return res
 
 # def step_by_step(input: str) -> str:
